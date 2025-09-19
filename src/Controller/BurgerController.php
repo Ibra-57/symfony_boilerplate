edac1d724868b6eda4af669dcfbb10e3925117e7
Ibrahim HMAMID <ibrahim57abc@gmail.com>
2025-09-19 17:12:03 +0200

    Exercice 1 part 1 et 2 terminer

---
+++ src/Controller/BurgerController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class BurgerController extends AbstractController
+{
+    #[Route('/burgers', name: 'burgers')]
+    public function list(): Response
+    {
+        return $this->render('burgers_list.html.twig');
+    }
+
+  /*  #[Route('/burgers/{id}', name: 'burgers')]
+    public function show(int $id): Response
+    {
+        return $this->render('burger_show.html.twig');
+    } */
+}
