1e1baf86c8dc6e0d81f99fefe98acd961522c527
Ibra-57 <ibrahim57abc@gmail.com>
2025-10-17 14:48:02 +0200

    fin de l'exercice 1

--- src/Controller/BurgerController.php
+++ src/Controller/BurgerController.php
@@ -8,15 +8,38 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class BurgerController extends AbstractController
 {
-    #[Route('/burgers', name: 'burgers')]
+    #[Route('/burgers', name: 'burgers_list')]
     public function list(): Response
     {
-        return $this->render('burgers_list.html.twig');
+        $burgers = [
+            1 => 'Cheeseburger',
+            2 => 'Bacon Burger',
+            3 => 'Vegan Burger',
+        ];
+
+        return $this->render('burgers_list.html.twig', [
+            'burgers' => $burgers,
+        ]);
     }
 
-  /*  #[Route('/burgers/{id}', name: 'burgers')]
+    #[Route('/burger/{id}', name: 'burger_show')]
     public function show(int $id): Response
     {
-        return $this->render('burger_show.html.twig');
-    } */
+        $burgers = [
+            1 => ['nom' => 'Cheeseburger', 'description' => 'Un délicieux burger avec du fromage fondant.'],
+            2 => ['nom' => 'Bacon Burger', 'description' => 'Un burger garni de bacon croustillant.'],
+            3 => ['nom' => 'Vegan Burger', 'description' => 'Un burger végétalien plein de saveur.'],
+        ];
+
+        if (!isset($burgers[$id])) {
+            return new Response("Burger avec l'ID $id non trouvé.", 404);
+        }
+
+        $burger = $burgers[$id];
+
+        return $this->render('burger_show.html.twig', [
+            'id' => $id,
+            'burger' => $burger,
+        ]);
+    }
 }
